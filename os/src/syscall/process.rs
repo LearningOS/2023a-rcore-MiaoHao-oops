98251d798fb17e33034a5690d9ac6613bc07ea20
MiaoHao-oops <haomiao19@mails.ucas.ac.cn>
2023-10-27 10:34:49 +0800

    update: process.rs
    - complete sys_get_time with virtual mem management
    
    authored by: Miao Hao

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -1,9 +1,12 @@
 //! Process management syscalls
+use core::mem::size_of;
+
 use crate::{
     config::MAX_SYSCALL_NUM,
     task::{
-        change_program_brk, exit_current_and_run_next, suspend_current_and_run_next, TaskStatus,
-    },
+        change_program_brk, exit_current_and_run_next, suspend_current_and_run_next, TaskStatus, current_user_token,
+    }, mm::translated_byte_buffer, 
+    timer::get_time_us,
 };
 
 #[repr(C)]
@@ -42,8 +45,23 @@ pub fn sys_yield() -> isize {
 /// HINT: You might reimplement it with virtual memory management.
 /// HINT: What if [`TimeVal`] is splitted by two pages ?
 pub fn sys_get_time(_ts: *mut TimeVal, _tz: usize) -> isize {
+    let mut kbuf = translated_byte_buffer(
+        current_user_token(),
+        _ts as *const u8 ,
+        size_of::<TimeVal>()
+    );
+    let usec = get_time_us();
+    let sec = usec / 1_000_000;
+    let usec = usec % 1_000_000;
+    let _ts = kbuf[0].as_mut_ptr() as *mut TimeVal;
+    unsafe {
+        *_ts = TimeVal {
+            sec,
+            usec,
+        };
+    }
     trace!("kernel: sys_get_time");
-    -1
+    0
 }
 
 /// YOUR JOB: Finish sys_task_info to pass testcases
